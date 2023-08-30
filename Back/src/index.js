08e9359b9fee1812b94e6293dea76ea521afe225
jorgelins <jorgelins1155@gmail.com>
2023-08-30 16:20:40 -0300

    start setup backend with prisma

---
+++ Back/src/index.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const app = express()
+const port = 3001
+
+// const spendingRouter = require('./routes/spending-routes')
+// require('./database/spending-db')
+const cors = require('cors');
+
+app.use(cors({
+  origin: '*'
+}));
+ 
+app.use(express.json());
+
+
+// app.use('/spending', spendingRouter)
+
+// require('./routes/spending-routes')(app);
+
+app.listen(port, () => {
+  console.log(`Iniciado na porta ${port}`)
+})
